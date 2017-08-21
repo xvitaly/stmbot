3f67369326e6e1c394316e14a215854b6c50cc1f
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-21 21:30:25 +0300

    Added basic functions.

--- src/ecbot.py
+++ src/ecbot.py
@@ -5,14 +5,30 @@ import telebot
 from settings import tgkey
 
 
+def runbot(key):
+    # Initialize bot...
+    bot = telebot.TeleBot(key)
+
+    # Initialize command handlers...
+    @bot.message_handler(commands=['start', 'help'])
+    def handlestart(message):
+        bot.send_message(message.chat.id, 'Приветствую вас!')
+
+    @bot.message_handler(func=lambda message: True, content_types=['text'])
+    def handleother(message):
+        bot.send_message(message.chat.id, message.text)
+
+    # Run bot forever...
+    bot.polling(none_stop=True)
+
 def main():
     try:
-        # Show message...
-        print(tgkey)
+        print('Launching bot with token %s...' % tgkey)
+        runbot(tgkey)
 
     except:
         # Exception detected...
-        print('An error occurred while querying backend.')
+        print('An error occurred while running bot!')
 
 
 if __name__ == '__main__':
