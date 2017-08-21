6fdbf242de621e2e5f892de0c70ae3c06e77fcd3
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-21 20:51:04 +0300

    Added basic and test contents to ecbot.py.

--- src/ecbot.py
+++ src/ecbot.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 # coding=utf-8
 
+
+def main():
+    try:
+        # Show message...
+        print('TEST')
+
+    except:
+        # Exception detected...
+        print('An error occurred while querying backend.')
+
+
+if __name__ == '__main__':
+    main()
