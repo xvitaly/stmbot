c51af5e56dfb326df6bd51140f2a259792b618ae
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-25 22:40:26 +0500

    Added new class method.

--- src/stchk.py
+++ src/stchk.py
@@ -2,8 +2,16 @@
 # coding=utf-8
 
 
+from urllib.request import urlopen
+
+
 class SteamChecker:
-    def __init__(self):
-        
-        self.SteamID32 = ''
+    def fetchxml(self):
+        apiuri = 'https://check.team-fortress.su/api.php?action=check&token=%s&id=%s' % (self._Token, self._ID)
+        with urlopen(apiuri) as xmlres:
+            return xmlres.read()
+
+    def __init__(self, tid, token):
+        self._ID = tid
+        self._Token = token
 
