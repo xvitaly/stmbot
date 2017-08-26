b2070c1bb2faf68a72c2ba42878afd6ea0e81bd6
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-26 19:38:28 +0500

    Added XML parser.

--- src/stchk.py
+++ src/stchk.py
@@ -3,6 +3,7 @@
 
 
 from urllib.request import urlopen
+from xml.dom import minidom
 
 
 class SteamChecker:
@@ -19,3 +20,20 @@ class SteamChecker:
         # Fetching XML from API...
         rxml = self._fetchxml()
 
+        # Parsing received XML...
+        xmlp = minidom.parse(rxml)
+        for up in xmlp.getElementsByTagName('userprofile'):
+            self.APIStatus = up.getElementsByTagName("qstatus")[0].firstChild.data
+            self.SteamID32 = up.getElementsByTagName("steamID")[0].firstChild.data
+            self.SteamID64 = up.getElementsByTagName("steamID64")[0].firstChild.data
+            self.SteamIDv3 = up.getElementsByTagName("steamIDv3")[0].firstChild.data
+            self.Nickname = up.getElementsByTagName("nickname")[0].firstChild.data
+            self.Avatar = up.getElementsByTagName("avatar")[0].firstChild.data
+            self.Description = up.getElementsByTagName("customdescr")[0].firstChild.data
+            self.SiteStatus = up.getElementsByTagName("sitestatus")[0].firstChild.data
+            self.VACStatus = up.getElementsByTagName("isbanned")[0].firstChild.data
+            self.F2PStatus = up.getElementsByTagName("isf2p")[0].firstChild.data
+            self.TradeStatus = up.getElementsByTagName("istrbanned")[0].firstChild.data
+            self.SRStatus = up.getElementsByTagName("steamrep")[0].firstChild.data
+            self.Premium = up.getElementsByTagName("ispremium")[0].firstChild.data
+            self.Permalink = up.getElementsByTagName("permalink")[0].firstChild.data
