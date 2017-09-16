551640c9700b4aa2408984f1563af4ae774a619c
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-16 23:21:50 +0500

    Added basic setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stmbot',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/xvitaly/stmbot',
+    license='GPLv3',
+    author='Vitaly Zaitsev',
+    author_email='vitaly@easycoding.org',
+    description='Simple Steam checker bot'
+)
