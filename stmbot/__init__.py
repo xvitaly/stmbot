8922ac1e58e6d74d6e4bf04293432e6603f67964
Vitaly Zaitsev <vitaly@easycoding.org>
2017-10-12 22:17:34 +0500

    Don't set package name.

--- stmbot/__init__.py
+++ stmbot/__init__.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from telebot import TeleBot
-from stmbot.checker import SteamChecker
+from .checker import SteamChecker
 
 
 class STMBot:
